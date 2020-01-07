6e95d80c069d3b18ce60173447eba100dd0d7f3d
lang-sky <skylang121@outlook.com>
2020-01-07 20:06:01 +0300

    register

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,8 +3,9 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -22,7 +23,7 @@ const Register = ({ setAlert }) => {
     if (password !== password2) {
       setAlert('Password do not match', 'danger', 2500);
     } else {
-      console.log('success');
+      register({ name, email, password });
     }
   };
 
@@ -40,16 +41,17 @@ const Register = ({ setAlert }) => {
             name="name"
             value={name}
             onChange={onChange}
-            required
+            // required
           />
         </div>
         <div className="form-group">
           <input
             type="email"
             placeholder="Email Address"
+            name="email"
             value={email}
             onChange={onChange}
-            name="email"
+            // required
           />
           <small className="form-text">
             This site uses Gravatar so if you want a profile image, use a
@@ -86,7 +88,8 @@ const Register = ({ setAlert }) => {
 };
 
 Register.propTypes = {
-  setAlert: PropTypes.func.isRequired
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired
 }
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
