3442a9cb34b723e6f0e637b35179c21f8793bed0
lang-sky <skylang121@outlook.com>
2020-01-08 00:17:14 +0300

    starting-dashboard

---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+import spinner from './spinner.png';
+
+const Spinner = () => {
+  return (
+    <Fragment>
+      <img
+        src={spinner}
+        style={{ width: '200px', margin: 'auto', display: 'block' }}
+        alt="Loading..."
+      />
+    </Fragment>
+  );
+};
+
+export default Spinner;
